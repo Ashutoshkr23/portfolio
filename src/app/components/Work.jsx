8a2550a885e1e53a4dbf8b819a2b71a6a8774fe9
Ashutoshkr23 <ashutoshkumar.ash205@gmail.com>
2024-01-07 17:53:07 +0530

    experience section fix

--- src/app/components/Work.jsx
+++ src/app/components/Work.jsx
@@ -7,11 +7,11 @@ const Work = () => {
 
   return (
     <section className="text-white" id="about">
-      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+      <div className="flex flex-col-reverse md:flex-row md:justify-between py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
         
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
-          <h2 className="text-4xl font-bold text-white mb-4">Experience</h2>
-          <p className="text-2xl text-gray-300 mt-8">Full Stack Developer <a href="https://www.alphamintlabs.com/" target="_blank" rel="noopener noreferrer"><span className="text-green-2 hover:text-[#60D9F9]">@ AlphamintLabs</span></a></p>
+          <h2 className="text-4xl font-bold text-white mb-2 mt-8">Experience</h2>
+          <p className="text-2xl text-gray-300 mt-4">Full Stack Developer <a href="https://www.alphamintlabs.com/" target="_blank" rel="noopener noreferrer"><span className="text-green-2 hover:text-[#60D9F9]">@ AlphamintLabs</span></a></p>
           <p className="text-sm opacity-80 mt-4">Dec 2022 - Present</p>
           <div className="flex gap-2 mt-4">
             <div className="h-6 w-4 ">
@@ -32,8 +32,8 @@ const Work = () => {
             <p>Started as a beginner and rapidly acquired proficiency in React, Next.js, and MongoDB while actively contributing to real-world projects, highlighting a commitment to continuous learning and skill development.</p>
           </div>
         </div>
-        <div className="hidden lg:block ml-16 rounded-[200px] w-[400px] h-[400px] overflow-hidden">
-        <Image src="/Images/Work.png" width={400} height={400} />
+        <div className="rounded-[200px] w-[300px] h-[300px] mx-auto overflow-hidden">
+        <Image src="/Images/Work.png" width={300} height={300} />
         </div>
       </div>
     </section>
